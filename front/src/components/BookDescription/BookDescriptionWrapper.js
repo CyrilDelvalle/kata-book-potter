f6998a0739f1f1ab99d1baca4f57a49d07d1230d
Cyril Delvalle <delvalle.cyril77@gmail.com>
2021-11-01 17:22:02 +0100

    add two last books and get 30 percents if all was buyed

--- front/src/components/BookDescription/BookDescriptionWrapper.js
+++ front/src/components/BookDescription/BookDescriptionWrapper.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const BookDescriptionWrapper = styled.div`
     position: absolute;
-    margin: 30rem 4rem 10rem 4rem;
+    margin: 30rem 4rem 10rem 30rem;
+    width: 60rem;
 `
 
 export default BookDescriptionWrapper;
--- front/src/components/BookList/BookListWrapper.js
+++ front/src/components/BookList/BookListWrapper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const BookListWrapper = styled.div`
 width: 70rem;
 gap: 1rem;
+margin-left: 8rem;
 display: flex;
 position: absolute;
 top: 0;
